6036c7ea9c62036aee43882c163348c0953b84fd
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-02-16 23:28:16 +0000

    started working on register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,47 @@
 
 <template>
+<div>
+<!-- <v-spacer></v-spacer> -->
+
   <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+  <h1>Register Component </h1>
+
+   <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+        >
+        <v-text-field
+            label="solo"
+            solo 
+            placeholder="Dense & Rounded"
+            filled
+            rounded
+            dense
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+   </v-card>
+   
+        </div>
 </template>
 
 <script>
