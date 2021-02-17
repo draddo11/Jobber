762cf0621f256607824d69b4bb421fb56fef82fa
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-02-17 22:57:25 +0000

    added email text field

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,6 @@
 
 <template>
-<div class="mx-auto">
+<div class="px-4 mx-auto">
 <!-- <v-spacer></v-spacer> -->
 <v-col
           cols="12"
@@ -14,6 +14,14 @@
   <h1>Register Component </h1>
   <h1>Register Component </h1>
   <h1>Signup with us to post and apply for Jobs </h1>
+  >
+    <br />
+    <p class="headline font-weight-black">
+      Sign In to manage your current Job boards.
+    </p>
+    <p class="caption font-weight-medium">
+      By continuing, you agree to our Terms of Use and Privacy Policy
+    </p>
  
 </v-col>
    <v-card
@@ -41,6 +49,12 @@
             single-line
             solo
             placeholder="LastName"
+          ></v-text-field>
+          <v-text-field
+            label="Email"
+            solo 
+            placeholder="Email"
+            dense
           ></v-text-field>
         </v-col>
       </v-row>
