55c7453bbe90798fe4160ca6253f92dfb487f35a
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-03-11 20:26:30 +0000

    added the firebase conifg

---
+++ src/firebaseApp.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB4vx2DkKyMLnvQINsdPbAUaM2FdIP753M",
+    authDomain: "jobber-95ff3.firebaseapp.com",
+    projectId: "jobber-95ff3",
+    storageBucket: "jobber-95ff3.appspot.com",
+    messagingSenderId: "295387123365",
+    appId: "1:295387123365:web:466e7ab8b2b6258ebdc74c",
+    measurementId: "G-HCR87GFV05"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
