ae85e6dbdbcbb454bc4fdbe4a733c95e2e244a6a
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-03-24 21:09:28 +0000

    setting up vue store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueStore from './store';
+import Store from './store/index';
 import  router from './router';
 import {firebaseApp} from './firebaseApp';
 
@@ -11,6 +12,7 @@ new Vue({
   vuetify,
   VueStore,
   router,
+  Store,
   firebaseApp,
   render: h => h(App)
 }).$mount('#app')
