ab23980da715f9e8e73f6cae0a891cb5e4e2436f
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-04-19 23:57:25 +0000

    updatinf actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,10 @@
 import * as types from './mutations-types'
 
-export const signin   = ({commit}, user_payload)=>{
+export const signIn   = ({commit}, user_payload)=>{
     commit(types.SIGN_IN , user_payload)
 }
 
-export const sigOut   = ({commit})=>{
+export const signOut   = ({commit})=>{
     commit(types.SIGN_OUT  )
 } 
 
