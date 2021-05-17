bb3b116e45603c2d578f389f23c7606fd8512db8
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-05-17 23:14:41 +0000

    job component eith Ts experiment

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -1,11 +1,34 @@
 <template>
-<div></div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge('30')">change age</button>
+  </div>
 </template>
 
-<script>
-export default {
-props:['jobs']
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+      return age
+    }
+  }
+});
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,43 @@ import vuetify from './plugins/vuetify';
 import store from './store';
 import  router from './router';
 import {firebaseApp} from './firebaseApp';
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge('30')">change age</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+      return age
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>
+
 
 Vue.config.productionTip = false
 
